6149f1216c6a66d67065be862ddf9e89efd5b116
Shraddha Suryawanshi <109404381+Shraddha4git@users.noreply.github.com>
2024-08-23 14:17:12 +0530

    Update backend.tf

--- book_shop_app/backend.tf
+++ book_shop_app/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "learning-terraform"
+    bucket = "learning-terraform-23-08"
     key    = "backend/books_shop_app.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamoDB-state-locking"
