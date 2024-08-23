1e988f5564a6edbd30a3d2157adacbc352db1436
Shraddha Suryawanshi <109404381+Shraddha4git@users.noreply.github.com>
2024-08-23 15:46:32 +0530

    Update main.tf

--- modules/key/main.tf
+++ modules/key/main.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "client_key" {
-    key_name = "client_key"
-    public_key = file("../modules/key/client_key.pub")
+    key_name = "id_rsa"
+    public_key = file("../modules/key/id_rsa.pub")
 }
 
